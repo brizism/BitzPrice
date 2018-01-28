9143b281aa6d8eb915aa067e613041f9a715a60b
brizism <montielbriseida@gmail.com>
2018-01-28 17:36:08 -0500

    add index and about components

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+const About = () => (
+  <div>
+    <h1>About BitzPrice</h1>
+    <p>Application to view Bitcoin prices</p>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+  <div>
+    <h1>Welcome to BitzPrice</h1>
+  </div>
+);
+
+export default Index;
